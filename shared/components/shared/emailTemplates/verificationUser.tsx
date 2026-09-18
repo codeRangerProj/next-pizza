0150f51bf5bc4ed2a205222562cc6d179c38eaab
codeRangerProj <h4mres@gmail.com>
2026-09-18 10:18:05 +0300

    fix

--- shared/components/shared/emailTemplates/verificationUser.tsx
+++ shared/components/shared/emailTemplates/verificationUser.tsx
@@ -5,11 +5,13 @@ interface Props {
 }
 
 export const VerificationUserTemplate: FC<Props> = ({code}) => {
+  const siteUrl = process.env.SITE_URL;
+
   return (
     <div>
       <p>Код подтверждения: <h2>{code}</h2></p>
 
-      <p><a href={`http://localhost:3000/api/auth/verify?code=${code}`}>Подтвердить регистрацию</a></p>
+      <p><a href={`${siteUrl}/api/auth/verify?code=${code}`}>Подтвердить регистрацию</a></p>
     </div>
   );
 }
